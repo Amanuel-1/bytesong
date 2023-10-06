2efc6e3170a0c4e6dd813049a5c142f28c626891
Amanuel-1 <amanuelgaromsa@gmail.com>
2023-10-06 18:05:58 +0300

    added emotion css

--- src/components/songs/songList.tsx
+++ src/components/songs/songList.tsx
@@ -6,7 +6,7 @@ const StyledImg  =  styled.img`
               width:140px;
               height:100%;
               object-fit:cover;
-              background-color:blue;
+              background-color:rgb(30,30,30);
               border-top-left-radius: 10px;
               border-bottom-left-radius: 10px;
 
@@ -16,9 +16,12 @@ const SongList = () => {
   return (
     <div className={styles.songPannel}>
         <div className={styles.songItem}>
-        <div className={styles.imageWrapper}>
-              <StyledImg src={require('../../assets/logo.png')} alt="image" />
-          </div>
+            <div className={styles.actions}>
+              actions
+            </div>
+            <div className={styles.imageWrapper}>
+                <StyledImg src={require('../../assets/logo.png')} alt="image" />
+            </div>
           <div className={styles.itemInfo}>
             <h1>Song One</h1>
             <small>Artist Abebe</small>
